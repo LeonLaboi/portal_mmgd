a9db325677305c07717bfc8e672cbeaa8e8026b0
LeonLaboi <leonel.laboissiere@gmail.com>
2024-10-09 19:22:32 -0300

    Update urls.py

--- galeria/urls.py
+++ galeria/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import index, alura  # Importando as views necessárias
 
 urlpatterns = [
-    path('', index),  # , name='index'), Página inicial
-    path('alura/', alura)  # , name='alura'), Página alura
+    path('', index, name='index') , #Página inicial
+    path('alura/', alura, name='alura')#, Página alura
 ]
