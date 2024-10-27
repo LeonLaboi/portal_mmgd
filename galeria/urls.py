c59ecc37dab5f482e34d55ee7a3fb63983842656
LeonLaboi <leonel.laboissiere@gmail.com>
2024-10-27 00:05:02 -0300

    Gráficos por perfis

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, alura, imagem  # Importando as views necessárias
+from .views import index, alura, imagem, get_graph  # Importando as views necessárias
 
 urlpatterns = [
     path('', index, name='index'), #Página inicial
     path('alura/', alura, name='alura'), #Página alura
     path('imagem/<int:perfil_id>', imagem, name='imagem'),
+    path('get_graph/<int:client_id>/', get_graph, name='get_graph'),
 ]
--- galeria/views.py
+++ galeria/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render, get_list_or_404
-# from django.http import HttpResponse
+import json
+import matplotlib.pyplot as plt
+import numpy as np
+import io
+import base64
+
+from django.shortcuts import render, get_object_or_404 #get_list_or_404
+from django.http import JsonResponse #HttpResponse
 from django.templatetags.static import static
 
-from galeria.models import Perfil
+from galeria.models import Perfil, LeituraCSV
+
 
 def index(request):
   
@@ -19,7 +26,87 @@ def alura(request):
     perfis = Perfil.objects.all()
     return render(request, 'galeria/alura.html', {'cards': perfis})
 
-def imagem(request, perfil_id):
-    perfil = get_list_or_404(Perfil, pk=perfil_id) #Perfil.objects.get(id=foto_id)
-    return render(request, 'galeria/imagem.html', {'perfil': perfil})
 
+def imagem(request, perfil_id):
+    perfil = get_object_or_404(Perfil, pk=perfil_id)
+
+    if perfil.nome == 'Gestor de Energia':
+        clientes = LeituraCSV.objects.values('id_client').distinct()
+        selected_filter = 'id_client'
+    else:
+        clientes = LeituraCSV.objects.values('id_uc').distinct()
+        selected_filter = 'id_uc'
+
+    selected_id = request.GET.get(selected_filter, clientes[0][selected_filter])
+    leituras = LeituraCSV.objects.filter(**{selected_filter: selected_id})
+
+    mes_refs = [leitura.mes_ref.strftime("%m/%Y") for leitura in leituras]
+    qtd_enrg_te = [leitura.qtd_enrg_te for leitura in leituras]
+
+    plt.figure(figsize=(10, 5))
+    plt.bar(mes_refs, qtd_enrg_te, color='skyblue')
+    plt.title(f'Quantidade de Energia por Mês para {selected_filter}={selected_id}')
+    plt.xlabel('Mês Referência')
+    plt.ylabel('Quantidade de Energia (kWh)')
+    plt.xticks(rotation=45)
+
+    buffer = io.BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    graphic = base64.b64encode(buffer.getvalue()).decode()
+    buffer.close()
+
+    return render(request, 'galeria/imagem.html', {
+        'perfil': perfil,
+        'graphic': graphic,
+        'clientes': clientes,
+        'selected_filter': selected_filter,
+        'selected_id': selected_id,
+    })
+
+
+def get_graph(request, client_id):
+    # Obtém os dados do gráfico para o cliente específico
+    leituras = LeituraCSV.objects.filter(id_client=client_id).values('mes_ref', 'qtd_enrg_te')
+
+    # Criação do gráfico
+    meses = [leitura['mes_ref'] for leitura in leituras]
+    consumos = [leitura['qtd_enrg_te'] for leitura in leituras]
+
+    plt.figure(figsize=(10, 5))
+    plt.bar(meses, consumos)
+    plt.xlabel('Meses')
+    plt.ylabel('Quantidade de Energia (kWh)')
+    plt.title(f'Consumo de Energia para Cliente ID {client_id}')
+
+    # Salvar o gráfico em um buffer
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    plt.close()
+    buf.seek(0)
+    
+    # Codificar o gráfico em base64
+    graphic = base64.b64encode(buf.read()).decode('utf-8')
+    return JsonResponse({'graphic': graphic})
+
+# def imagem(request, perfil_id):
+#     perfil = get_list_or_404(Perfil, pk=perfil_id) #Perfil.objects.get(id=foto_id)
+#     return render(request, 'galeria/imagem.html', {'perfil': perfil})
+
+# def imagem_view(request):
+#     id_cliente = request.GET.get('id_cliente', None)
+#     if id_cliente:
+#         dados = LeituraCSV.objects.filter(id_cliente=id_cliente).values('mes_ref', 'qtd_enrg_te')
+#     else:
+#         dados = LeituraCSV.objects.all().values('mes_ref', 'qtd_enrg_te', 'id_cliente')
+
+#     # Converte os dados para JSON
+#     dados_json = json.dumps(list(dados))
+
+#     # Busca todos os IDs únicos de cliente para o campo de seleção
+#     ids_clientes = LeituraCSV.objects.values_list('id_cliente', flat=True).distinct()
+
+#     return render(request, 'galeria/imagem.html', {
+#         'dados': dados_json,
+#         'ids_clientes': ids_clientes,
+#     })
